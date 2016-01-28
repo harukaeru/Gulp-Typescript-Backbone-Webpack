7e88025415160fcb7faac017171810188f7a6f09
(_| |_) <kaeru@kaeru.kaeru>
2016-01-28 11:02:06 +0900

    all task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var typescript = require('gulp-typescript');
 
+gulp.task('build', ['ts']);
+// Because gulp.run is deprecated, don't use gulp.run
+
 gulp.task('ts', function() {
     var options = {
         out: 'index.js'
